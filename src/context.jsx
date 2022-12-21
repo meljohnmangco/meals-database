aea0a04395f32f19942dde75653af1d43d2be17a
landeroov <meljohn7.mm@gmail.com>
2022-12-22 07:50:22 +0800

    axios data fetching

--- src/context.jsx
+++ src/context.jsx
@@ -1,23 +1,24 @@
 import React, {useContext, useEffect} from 'react';
+import axios from 'axios';
 
 const AppContext = React.createContext()
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({ children }) => {
 
-	const fetchData = async () => {
+	const fetchMeals = async (url) => {
 		try {
-			const response = await fetch('https://randomuser.me/api/')
-			const data = await response.json()
+			const {data} = await axios(url)
 			console.log(data)
 		} catch(error) {
-			console.log(error)
-
+			console.log(error.response)
 		}
 	}
 
-	useEffect(() => {
-		
-		fetchData();
+	useEffect(() => {		
+		fetchMeals(allMealsUrl);
 	},[])
 
 	return <AppContext.Provider value={{name: 'John', role: 'Student'}}>
